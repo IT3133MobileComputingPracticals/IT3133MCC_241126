05b5c44d3daf5efdb5e0161a1e8e827435e5f009
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-11-26 09:37:55 +0000

    included username and Password fields

---
+++ myapp/src/components/Login.js
@@ -0,0 +1,20 @@
+import {useNavigate} from 'react-router-dom'
+
+export default function Login(){
+    
+    const navigate = useNavigate();
+    const authenticate=()=>{
+        navigate('/dash')
+    }
+
+    return(
+        <>
+            <div>
+                <h1>Login Here: </h1>
+                <div>Username:<input type='text' name='username'/></div>
+                <div>Password:<input type='password' name='password'/></div>
+                <button onClick={authenticate}>Login</button>
+            </div>
+        </>
+    )
+}
